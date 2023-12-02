5e7a9d70d9be40ba042478340cee453d4300dc2a
Rahul <ry03032003@gmail.com>
2023-12-02 22:19:38 +0530

    first commit

---
+++ valid.js
@@ -0,0 +1,36 @@
+function myfunction(){
+    var x =document.getElementById("pass");
+
+    if(x.type === "password"){
+        x.type = "text";
+    }
+    else{
+        x.type = "password";
+    }
+}
+
+function validate(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if(password.value.length >= 8){
+        alert("Login Succesfull");
+        window.location.replace("index.html");
+        return false;
+    }
+    else{
+        alert("Login Failed");
+    }
+}
+
+
+function validate1(){
+    
+    window.location.replace("indexss.html");
+}
+
+
+
+function lpage(){
+    window.location.replace("Landingpage.html")
+}
